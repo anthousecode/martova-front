280218ed41743a61255893d7375f0e9914915311
Sergioramasano <razumenko.serg@gmail.com>
2019-11-24 22:46:44 +0200

    navbar in process

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,89 +1,34 @@
 <template>
   <nav class="navbar navbar-expand-lg navbar-dark py-2 navbar-custom col">
-    <nuxt-link class="navbar-brand" to="/">Nuxt SSR</nuxt-link>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <nuxt-link class="navbar-brand" to="/">efwefw</nuxt-link>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div
       class="collapse navbar-collapse"
       id="navbarSupportedContent"
     >
+      <ul>
+        <li><a href="#"></a></li>
+        <li><a href="#"></a></li>
+        <li><a href="#"></a></li>
+      </ul>
       <ul class="navbar-nav ml-auto">
-        <li class="nav-item">
+        <li
+          v-for="item of menuItems.slice(5)"
+          :key="item.id"
+          class="nav-item"
+        >
           <nuxt-link
             exact
             no-prefetch
             active-class="active"
-            class="nav-link"
+            class="nav-link py-0"
             to="/"
           >
-            Продажа участков
-            <span
-              class="sr-only">(current)
-            </span>
+            {{item.username}}
           </nuxt-link>
-        </li>
-        <li class="nav-item">
-          <nuxt-link
-            active-class="active"
-            class="nav-link"
-            to="/infrastructure">
-            Инфраструктура
-          </nuxt-link>
-
-        </li> <li class="nav-item">
-          <nuxt-link
-            active-class="active"
-            class="nav-link"
-            to="/location">
-            Расположение
-          </nuxt-link>
-
-        </li>
-        <li class="nav-item">
-          <nuxt-link
-            active-class="active"
-            class="nav-link"
-            to="/gallery">
-            Галерея
-          </nuxt-link>
-
-        </li>
-        <li class="nav-item">
-          <nuxt-link
-            active-class="active"
-            class="nav-link"
-            to="/contacts">
-            Контакты
-          </nuxt-link>
-        </li>
-        <li class="nav-item">
-          <nuxt-link
-            active-class="active"
-            class="nav-link"
-            to="/news">
-            Новости
-          </nuxt-link>
-        </li>
-        <li
-          v-if="!hasToken"
-          class="nav-item">
-          <nuxt-link
-            active-class="active"
-            class="nav-link"
-            to="/login">
-            login
-          </nuxt-link>
-        </li>
-        <li class="nav-item" v-else>
-          <a
-            @click.prevent="logout"
-            active-class="active"
-            class="nav-link"
-            href="#"
-           >
-            Logout
-          </a>
         </li>
       </ul>
     </div>
@@ -91,29 +36,30 @@
 </template>
 
 <script>
-    export default {
-        name: "Navbar",
-        computed:{
-          hasToken(){
-              return this.$store.getters.hasToken
-          }
-        },
-        methods:{
-            logout(){
-                this.$store.dispatch('logout');
-                this.$router.push('/login')
-            }
-        }
-    }
+  export default{
+    data:()=>({
+      menuItems:[]
+    }),
+    async mounted(){
+        this.menuItems = await this.$axios.$get(`https://jsonplaceholder.typicode.com/users`)
+      }
+  }
 </script>
 
 <style scoped lang="scss">
- .navbar-custom{
-   z-index: 1;
-   opacity: .8;
-   background-color: black;
-   color:white;
-   position: fixed;
-   top: 0;
- }
+  .navbar-custom {
+    z-index: 1;
+    opacity: .8;
+    background-color: black;
+    color: white;
+    position: fixed;
+    top: 0;
+    li{
+      text-transform: uppercase;
+      line-height: 14px;
+      &:first-child{
+        border-right:1px solid white;
+      }
+    }
+  }
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -7,6 +7,8 @@
 </template>
 
 <script>
+  export default{
+  }
 </script>
 
 <style scoped lang="scss">
